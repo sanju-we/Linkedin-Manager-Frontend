a75c7e8730a3dc20e6b925fd4469bd9c43b68a72
sanju-we <pnsanju8@gmail.com>
2025-12-29 19:37:36 +0530

    fixes

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,7 +10,7 @@ export async function middleware(req: NextRequest) {
   }
 
   try {
-    const res = await fetch(`${process.env.API_URL}/auth/verify`, {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/verify`, {
       headers: {
         cookie: req.headers.get('cookie') || '',
       },
